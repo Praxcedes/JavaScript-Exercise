40c582721d73e3c201d304edf1609c08e2919013
Praxcedes <praxcedeskabeya@gmail.com>
2025-06-18 13:59:05 -0700

    JavaScript-Exercise

--- practice4.js
+++ practice4.js
@@ -2,7 +2,5 @@
 function checkEvenOrOdd(number) {
   return number % 2 === 0 ? "Even" : "Odd";
 }
-
-// Example usage
 console.log(checkEvenOrOdd(6)); // "Even"
 console.log(checkEvenOrOdd(17));  // "Odd"
