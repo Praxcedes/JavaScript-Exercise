244f7d22662057bcd2dc08f0427ad07f3c1195e2
Praxcedes <praxcedeskabeya@gmail.com>
2025-06-18 13:46:51 -0700

    JavaScript-Exercise

---
+++ practice4.js
@@ -0,0 +1,8 @@
+// Function to check if a number is Even or Odd
+function checkEvenOrOdd(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
+
+// Example usage
+console.log(checkEvenOrOdd(6)); // "Even"
+console.log(checkEvenOrOdd(17));  // "Odd"
