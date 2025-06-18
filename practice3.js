0df712cea24e28e1fb98171743db6a4c1196d317
Praxcedes <praxcedeskabeya@gmail.com>
2025-06-18 13:37:39 -0700

    JavaScript-Exercise

---
+++ practice3.js
@@ -0,0 +1,8 @@
+function calculateCanvasArea(length, width) {
+  const area = length * width;
+  console.log(`🎨 Your rectangle's canvas area is: ${area} square units.`);
+  return area;
+}
+
+// Example usage:
+calculateCanvasArea(5, 8); // 🎨 Your rectangle's canvas area is:40 square units.
